735fc2e0001bf35d683a9f0a326c84eac317d6c8
isaacruz2308 <155272270+isaacruz2308@users.noreply.github.com>
2024-11-02 15:39:37 -0400

    Added coments and user input

--- Rotor_Machine.py
+++ Rotor_Machine.py
@@ -1,9 +1,12 @@
 class RotorMachine: 
+
+    # Initialize the Rotor Machine
     def __init__(self):
         self.abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         self.rotors = ['EKMFLGDQVZNTOWYHXUSPAIBRCJ', 'AJDKSIRUXBLHWTMCQGZNPYFVOE', 'BDFHJLCPRTXVZNYEIWGAKMUSQO']
         self.rotation_counters = [0, 0, 0]
 
+    # Handling rotors rotations
     def rotate(self, index):
         self.rotors[index] = self.rotors[index][1:] + self.rotors[index][0]
         self.rotation_counters[index] += 1
@@ -15,6 +18,7 @@ class RotorMachine:
             self.rotation_counters[1] = 0
             self.rotate(2)
     
+    # Encrypt a single letter
     def single_letter_encrypt(self, letter):
         for rotor in self.rotors:
             letter = rotor[self.abc.find(letter)]
@@ -22,6 +26,7 @@ class RotorMachine:
         self.rotate(0)
         return letter
     
+    # Call Single letter encryption for every char in the message
     def encrypt_message(self, message):
         encryption = ""
         for letter in message:
@@ -32,13 +37,15 @@ class RotorMachine:
         
         return encryption
     
+    # Decrypt a single letter
     def single_letter_decrypt(self, letter):
         for rotor in reversed(self.rotors):
             letter = self.abc[rotor.find(letter)]
 
         self.rotate(0)
         return letter
-
+    
+    # Call single letter decryption for every char in the message
     def decrypt_message(self, message):
         decryption = ""
         for letter in message:
@@ -51,11 +58,11 @@ class RotorMachine:
 
 
 # Run code
+message = input("Enter some text: ")
+rotorMachine = RotorMachine()
+encrypted_msg = rotorMachine.encrypt_message(message)
 
-message1 = "Hello World"
-rotorMachine = RotorMachine()
-encrypted_msg = rotorMachine.encrypt_message(message1)
-rotorMachine = RotorMachine()
+rotorMachine = RotorMachine() # Have to re-initialize the rotor machine for effective decryption
 decrypted_msg = rotorMachine.decrypt_message(encrypted_msg)
 print("Encrypted Message: ", encrypted_msg)
 print("")
